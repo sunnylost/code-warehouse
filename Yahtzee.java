3f02175f0c8e8a694f7224275479b7ca08a33608
sunnylost <sunnylost@gmail.com>
2013-08-12 16:09:59 +0800

    Update Yahtzee.java

--- Yahtzee.java
+++ Yahtzee.java
@@ -1,11 +1,17 @@
 import java.util.Arrays;
 
 /*
-This task is about the scoring in the first phase of the die-game Yahtzee, where five dice are used. The score is determined by the values on the upward die faces after a roll. The player gets to choose a value, and all dice that show the chosen value are considered active. The score is simply the sum of values on active dice.
+This task is about the scoring in the first phase of the die-game Yahtzee, where five dice are used. 
+The score is determined by the values on the upward die faces after a roll. 
+The player gets to choose a value, and all dice that show the chosen value are considered active.
+The score is simply the sum of values on active dice.
 
-Say, for instance, that a player ends up with the die faces showing 2, 2, 3, 5 and 4. Choosing the value two makes the dice showing 2 active and yields a score of 2 + 2 = 4, while choosing 5 makes the one die showing 5 active, yielding a score of 5.
+Say, for instance, that a player ends up with the die faces showing 2, 2, 3, 5 and 4. 
+Choosing the value two makes the dice showing 2 active and yields a score of 2 + 2 = 4, 
+while choosing 5 makes the one die showing 5 active, yielding a score of 5.
 
-Your method will take as input an int[] toss, where each element represents the upward face of a die, and return the maximum possible score with these values.
+Your method will take as input an int[] toss, where each element represents the upward face of a die, 
+and return the maximum possible score with these values.
 */
 public class Yahtzee {
   int maxPoints(int[] toss) {
