a8636d0797f4361ec57d9005eb36f8ae2cb9a786
sunnylost <sunnylost@gmail.com>
2013-08-12 16:09:30 +0800

    Update DitherCounter.java

--- DitherCounter.java
+++ DitherCounter.java
@@ -2,9 +2,16 @@ import java.util.Arrays;
 import java.util.regex.*;
 
 /*
-Sometimes when computer programs have a limited number of colors to use, they use a technique called dithering. Dithering is when you use a pattern made up of different colors such that when the colors are viewed together, they appear like another color. For example, you can use a checkerboard pattern of black and white pixels to achieve the illusion of gray.
+Sometimes when computer programs have a limited number of colors to use, they use a technique called dithering. 
+Dithering is when you use a pattern made up of different colors such that when the colors are viewed together, 
+they appear like another color. For example, you can use a checkerboard pattern of black and white pixels to achieve 
+the illusion of gray.
 
-You are writing a program to determine how much of the screen is covered by a certain dithered color. Given a computer screen with each pixel being a certain color, and a list of all the solid colors that make up the dithered color, return the number of pixels on the screen that are used to make up the dithered color. Each pixel will be represented by a character in screen. Each character in screen and in dithered will be an uppercase letter ('A'-'Z') representing a color.
+You are writing a program to determine how much of the screen is covered by a certain dithered color. 
+Given a computer screen with each pixel being a certain color, and a list of all the solid colors that make up 
+the dithered color, return the number of pixels on the screen that are used to make up the dithered color. 
+Each pixel will be represented by a character in screen. 
+Each character in screen and in dithered will be an uppercase letter ('A'-'Z') representing a color.
 
 Assume that any pixel which is a color contained in dithered is part of the dithered color.
 */
