23894a983f1495ef685eadd7b678caa959056f48
sunnylost <sunnylost@gmail.com>
2014-03-07 23:34:03 +0800

    用加法来求乘积，对数步骤

---
+++ sicp/1.17.rkt
@@ -0,0 +1,16 @@
+#lang racket
+(define (* a b)
+  (cond ((= b 0) 0)
+        ((even? b) (* (double a) (halve b)))
+        (else (+ a (* a (- b 1))))))
+
+(define (double n)
+  (+ n n))
+
+(define (halve n)
+  (/ n 2))
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(* 2 16)
