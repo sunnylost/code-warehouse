9e9ad23e668d2c46711a793bf5223cf07e7527c1
sunnylost <sunnylost@gmail.com>
2014-03-08 00:34:20 +0800

    迭代求乘积，基于 1.16 和 1.17
    思路还卡住了……大晚上不能写程序啊！

---
+++ sicp/1.18.rkt
@@ -0,0 +1,21 @@
+#lang racket
+(define (* a b)
+  (define (inner-mult a b r)
+    (cond ((= 0 a) 0)
+          ((= 1 a) (+ b r))
+          ((even? a) (inner-mult (halve a) (double b) r))
+          (else (inner-mult (- a 1) b (+ r b)))))
+  
+  (inner-mult a b 0)
+)
+
+(define (double n)
+  (+ n n))
+
+(define (halve n)
+  (/ n 2))
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(* 13 6)
