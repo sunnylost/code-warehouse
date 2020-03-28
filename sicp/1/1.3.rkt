9b0a6ddb34a804caf8c6bbcf5ee530208297a8be
sunnylost <sunnylost@gmail.com>
2020-03-29 02:02:07 +0800

    update

--- sicp/1/1.3.rkt
+++ sicp/1/1.3.rkt
@@ -2,16 +2,23 @@
 
 ;Exercise 1.3: Define a procedure that takes three numbers as arguments and returns the sum of the squares of the two larger numbers.
 
-(define (square a b)
-    (+ (* a a) (* b b))
-)
+(define (square x)
+    (* x x)
+  )
+
+(define (square-sum a b)
+  (+
+   (square a)
+   (square b)
+     )
+  )
 
 (define (sum-of-two a b c)
     (cond 
-        ((and (> a b) (> c b)) (square a c))
-        ((and (> a c) (> b c)) (square a b))
-        (else (square b c))
+        ((and (> a b) (> c b)) (square-sum a c))
+        ((and (> a c) (> b c)) (square-sum a b))
+        (else (square-sum b c))
     )
-)
+  )
 
 (sum-of-two 4 3 5)
--- sicp/2/2.6.rkt
+++ sicp/2/2.6.rkt
@@ -1,9 +1,26 @@
 #lang racket
 
-(define zero (lambda (f) (lambda (x) x))) 
+(define zero
+  (lambda (f)
+    (lambda (x) x)))
 
 (define (add-1 n)
-    (lambda (f) (lambda (x) (f ((n f) x)))))
+  (lambda (f)
+    (lambda (x)
+      (f ((n f) x)
+         )
+      )
+    )
+  )
+
+(define (add-2 n)
+  (lambda (f)
+    (lambda (x)
+      (f
+       (f ((n f) x)))
+      )
+    )
+  )
 
 (define one
     (lambda (f) (lambda (x) (f x))))
@@ -11,18 +28,37 @@
 (define two
     (lambda (f) (lambda (x) (f (f x)))))
 
-(define (add a b)
-    (lambda (f)
-        (lambda (x) (
-            (a f) ((b f) x)
-        ))
-    )
-)
+(define three
+    (lambda (f) (lambda (x) (f (f (f x))))))
 
-(define a (
-    ((add (add-1 two) (add-1 two)) (lambda (x) 
-        (print x) 
+(define (add a b)
+  (lambda (f)
+    (lambda (x)
+      (
+       (b f) ((a f) x))
+      )
+    )
+  )
+
+(define (mul a b)
+  (lambda (f)
+    (lambda (x)
+      ((a (b f)) x)
+      )
+    )
+  )
+
+(define (power a b)
+  (lambda (f)
+    (lambda (x)
+      (((b a) f) x)
+      )
+    )
+  )
+
+(((power three three) (lambda (x) 
+        (print x)
         (newline)
         x))
-    "aaa")
+ "a"
 )
